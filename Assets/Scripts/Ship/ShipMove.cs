253e0832317cfba12173b25dc55eec2b94370875
KIT <kit311290@gmail.com>
2025-06-18 10:30:39 +0300

    Create

---
+++ Assets/Scripts/Ship/ShipMove.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class ShipMove : MonoBehaviour
+{
+    private void Start()// TEMPIRARY
+    {
+        Initialize();
+    }
+
+    public void Initialize()
+    {
+        
+    }
+}
