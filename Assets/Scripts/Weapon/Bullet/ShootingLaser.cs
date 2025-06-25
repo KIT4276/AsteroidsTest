4f12e99f77117a42488859ec1af62b7dee8f65b8
KIT <kit311290@gmail.com>
2025-06-25 12:23:46 +0300

    add: limited number of laser shots; Recovery laser shots number

--- Assets/Scripts/Weapon/Bullet/ShootingLaser.cs
+++ Assets/Scripts/Weapon/Bullet/ShootingLaser.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 using Zenject;
 
 public class ShootingLaser : MonoBehaviour
@@ -9,31 +11,65 @@ public class ShootingLaser : MonoBehaviour
     [SerializeField] private LayerMask _layerMask;
     [SerializeField] private float _distance = 100;
     [SerializeField] private float _laserThickness = 0.5f;
+    [Space]
+    [SerializeField] private int _numberOfShots = 5;
+    [SerializeField] private float _oneShotRecoveryTime = 15;
 
     private IInputHandler _inputHandler;
+    private float _timer;
+    public int ShotsLeft { get; private set; }
 
     [Inject]
     private void Initialize(IInputHandler inputHandler)
     {
         _inputHandler = inputHandler;
         inputHandler.LaserFireAction += LaserFire;
+
+        ShotsLeft = _numberOfShots;
+        _timer = _oneShotRecoveryTime;
     }
 
     private void LaserFire()
     {
-        RaycastHit2D[] hits = Physics2D
-            .CircleCastAll(_gunBarrel.position, _laserThickness, _gunBarrel.up, _distance, _layerMask);
-
-        foreach (var hit in hits)
+        if (ShotsLeft > 0)
         {
-            if (hit.collider != null && hit.collider.TryGetComponent<IDamageable>(out var damageable))
-            {
-                damageable.TakeBulletDamage();
-            }
-        }
+            RaycastHit2D[] hits = Physics2D
+                .CircleCastAll(_gunBarrel.position, _laserThickness, _gunBarrel.up, _distance, _layerMask);
 
-        StartCoroutine(_shootingLaserRenderer
-            .LaserEffect(_gunBarrel.position, _gunBarrel.position + _gunBarrel.up* _distance, _laserThickness));
+            foreach (var hit in hits)
+            {
+                if (hit.collider != null && hit.collider.TryGetComponent<IDamageable>(out var damageable))
+                {
+                    damageable.TakeBulletDamage();
+                }
+            }
+
+            if (_shootingLaserRenderer != null)
+            {
+                StartCoroutine(_shootingLaserRenderer
+                    .LaserEffect(_gunBarrel.position, _gunBarrel.position + _gunBarrel.up * _distance, _laserThickness));
+            }
+            ShotsLeft--;
+            
+            if (ShotsLeft < 0)
+            {
+                ShotsLeft = 0;
+            }
+
+            StartCoroutine(RecoveryRoutine());
+        }
+    }
+
+    private IEnumerator RecoveryRoutine()
+    {
+        yield return new WaitForSeconds(_oneShotRecoveryTime);
+
+        ShotsLeft++;
+        
+        if (ShotsLeft > _numberOfShots)
+        {
+            ShotsLeft = _numberOfShots;
+        }
     }
 
     private void OnDestroy()
